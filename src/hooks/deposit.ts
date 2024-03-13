26051963cfaff804ed9ce2e700e8f73bdbe74271
skyehy5131 <skye.hy@etransfer.exchange>
2024-03-13 20:05:47 +0800

    fix: init tokenList to compatible with old logic

--- src/hooks/deposit.ts
+++ src/hooks/deposit.ts
@@ -1,7 +1,13 @@
 import { useTokenState } from 'store/Provider/hooks';
 import { BusinessType } from 'types/api';
+import { initDepositTokenState } from 'store/reducers/token/slice';
 
 export function useDeposit() {
   const tokenState = useTokenState();
+  // compatible with old logic add init value
+  if (!tokenState[BusinessType.Deposit]) {
+    tokenState[BusinessType.Deposit] = initDepositTokenState;
+  }
+
   return tokenState[BusinessType.Deposit];
 }
--- src/hooks/withdraw.ts
+++ src/hooks/withdraw.ts
@@ -1,7 +1,13 @@
 import { useTokenState } from 'store/Provider/hooks';
 import { BusinessType } from 'types/api';
+import { initWithdrawTokenState } from 'store/reducers/token/slice';
 
 export function useWithdraw() {
   const tokenState = useTokenState();
+  // compatible with old logic add init value
+  if (!tokenState[BusinessType.Withdraw]) {
+    tokenState[BusinessType.Withdraw] = initWithdrawTokenState;
+  }
+
   return tokenState[BusinessType.Withdraw];
 }
