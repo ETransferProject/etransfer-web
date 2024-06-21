23ffac23cda6c3be1390fd4565f5b8c1ba16e1ea
doraemon <doraemon@etransfer.exchange>
2024-06-21 11:51:55 +0800

    feat: 🎸 add preloading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
 'use client';
+import CircleLoading from 'components/CircleLoading';
 import dynamic from 'next/dynamic';
 
-export default dynamic(() => import('pageComponents/Home'));
+export default dynamic(() => import('pageComponents/Home'), {
+  loading: () => (
+    <div className="row-center pre-loading">
+      <CircleLoading />
+    </div>
+  ),
+});
