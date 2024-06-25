acd4cde6ed719557a4eeb0e5b47372feff4ea6dd
doraemon <doraemon@etransfer.exchange>
2024-06-25 15:03:10 +0800

    feat: 🎸 format token display

--- src/pageComponents/DepositContent/DepositTip/index.tsx
+++ src/pageComponents/DepositContent/DepositTip/index.tsx
@@ -1,6 +1,7 @@
 import { InfoBrandIcon } from 'assets/images';
 import styles from './styles.module.scss';
 import clsx from 'clsx';
+import { formatSymbolDisplay } from 'utils/format';
 
 type TDepositTipProps = {
   fromToken: string;
@@ -14,9 +15,9 @@ export default function DepositTip({ fromToken, toToken, isShowIcon = true }: TD
       {isShowIcon && <InfoBrandIcon className="flex-shrink-0" />}
       <span className={styles['text']}>
         <span>{`Deposit `}</span>
-        <span className={styles['token']}>{fromToken}</span>
+        <span className={styles['token']}>{formatSymbolDisplay(fromToken)}</span>
         <span>{` to the following address to receive `}</span>
-        <span className={styles['token']}>{toToken}</span>
+        <span className={styles['token']}>{formatSymbolDisplay(toToken)}</span>
         <span>{` in your connected wallet on aelf chain.`}</span>
       </span>
     </div>
