335db47e5ca6aaca73c7c60a311ca31bf7ed9777
doraemon <doraemon@etransfer.exchange>
2024-07-18 11:07:35 +0800

    feat: 🎸 format amount for detail

--- src/pageComponents/InfoPage/TransferDetail/components/TokenAmount/index.tsx
+++ src/pageComponents/InfoPage/TransferDetail/components/TokenAmount/index.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.scss';
 import clsx from 'clsx';
 import useGetTokenIcon from 'hooks/infoDashboard';
 import DisplayImage from 'components/DisplayImage';
-import { formatSymbolDisplay } from 'utils/format';
+import { ZERO, formatSymbolDisplay } from 'utils/format';
 import { useMemo } from 'react';
 
 export default function TokenAmount({
@@ -20,6 +20,20 @@ export default function TokenAmount({
   const iconBackup = getTokenIcon(symbol)?.icon || '';
   const formatSymbol = useMemo(() => formatSymbolDisplay(symbol), [symbol]);
 
+  const formatAmount = useMemo(() => {
+    if (ZERO.plus(amount).isLessThan(ZERO.plus('0.0001'))) {
+      return '<0.0001';
+    }
+    return amount;
+  }, [amount]);
+
+  const formatUsd = useMemo(() => {
+    if (ZERO.plus(amountUsd).isLessThan(ZERO.plus('0.01'))) {
+      return '<$0.01';
+    }
+    return `$${amountUsd}`;
+  }, [amountUsd]);
+
   return (
     <div className={clsx('flex-row-center', styles['token-amount'])}>
       <div className="flex-row-center">
@@ -32,10 +46,10 @@ export default function TokenAmount({
           width={16}
           height={16}
         />
-        <span className={styles['token-amount-value']}>{amount}</span>
+        <span className={styles['token-amount-value']}>{formatAmount}</span>
         <span className={styles['token-amount-symbol']}>{` ${formatSymbol}`}</span>
       </div>
-      <div className={styles['token-amount-usd']}>{`$${amountUsd}`}</div>
+      <div className={styles['token-amount-usd']}>{formatUsd}</div>
     </div>
   );
 }
