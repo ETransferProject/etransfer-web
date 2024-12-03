a08754b4de9da54708ff9189903be6cb334353da
doraemon <doraemon@etransfer.exchange>
2024-12-03 14:02:13 +0800

    refactor: update aelf-web-login

--- src/provider/webLoginV2Provider.tsx
+++ src/provider/webLoginV2Provider.tsx
@@ -1,10 +1,8 @@
 'use client';
 import '../utils/telegram-web-app';
-import { WebLoginProvider, init } from '@aelf-web-login/wallet-adapter-react';
+import { WebLoginProvider } from '@aelf-web-login/wallet-adapter-react';
 import { config } from './webLoginV2Config';
 
 export default function WebLoginV2Providers({ children }: { children: React.ReactNode }) {
-  const bridgeAPI = init(config); // upper config
-
-  return <WebLoginProvider bridgeAPI={bridgeAPI}>{children}</WebLoginProvider>;
+  return <WebLoginProvider config={config}>{children}</WebLoginProvider>;
 }
