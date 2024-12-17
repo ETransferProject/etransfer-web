8715c8ae95828e9b0384cf2d9fb5c3690497f351
doraemon <doraemon@etransfer.exchange>
2024-12-17 13:54:08 +0800

    fix: type error

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -81,8 +81,10 @@ export const replaceCharacter = (str: string, replaced: string, replacedBy: stri
 };
 
 export const formatSymbolDisplay = (str: string) => {
+  if (!str) return '';
+
   // Prevent malicious tampering of the token display issued by users
-  if (str.includes('SGR-1')) return replaceCharacter(str, '-1', '');
+  if (str?.includes('SGR-1')) return replaceCharacter(str, '-1', '');
 
   return str;
 };
