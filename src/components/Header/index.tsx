6dee5bd8bf0b6bf0e682136e08cc0cd5563ed7fa
quast <quast@etransfer.exchange>
2024-12-21 16:01:49 +0800

    style: fix meta

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,10 +22,9 @@ export function DefaultHead({ title = BRAND_NAME, description }: DefaultHeadProp
   return (
     <Head>
       <title>{title}</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta
         name="viewport"
-        content="width=device-width,height=device-height,inital-scale=1.0,maximum-scale=1.0,user-scalable=no;"
+        content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=no;"
       />
       <meta name="description" content={description || title} />
     </Head>
