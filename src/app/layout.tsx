507426bcb66abd53ace867a13188ff8822026f6e
doraemon <doraemon@etransfer.exchange>
2024-12-21 17:08:41 +0800

    fix: page zoom issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,18 @@ import '@etransfer/ui-react/dist/assets/index.css';
 
 import Provider from 'provider';
 import { BRAND_NAME } from 'constants/index';
-import { Metadata } from 'next';
+import { Metadata, Viewport } from 'next';
 
 export const metadata: Metadata = {
   title: BRAND_NAME,
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  height: 'device-height',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,12 @@
-import Head from 'next/head';
 import React from 'react';
 import MobileHeader from './MobileHeader';
 import WebHeader from './WebHeader';
 import { useCommonState } from 'store/Provider/hooks';
 import styles from './styles.module.scss';
-import { BRAND_NAME } from 'constants/index';
 
-export type DefaultHeadProps = { title?: string; description?: string };
-
-export default function Header(props: DefaultHeadProps) {
+export default function Header() {
   const { isPadPX } = useCommonState();
   return (
-    <div className={styles['header-wrapper']}>
-      <DefaultHead {...props} />
-      {isPadPX ? <MobileHeader /> : <WebHeader />}
-    </div>
-  );
-}
-
-export function DefaultHead({ title = BRAND_NAME, description }: DefaultHeadProps) {
-  return (
-    <Head>
-      <title>{title}</title>
-      <meta
-        name="viewport"
-        content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=no;"
-      />
-      <meta name="description" content={description || title} />
-    </Head>
+    <div className={styles['header-wrapper']}>{isPadPX ? <MobileHeader /> : <WebHeader />}</div>
   );
 }
