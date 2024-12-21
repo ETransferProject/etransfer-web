9eb6db27fb8a6e5ac69f07ed2063fe317adf11e4
doraemon <doraemon@etransfer.exchange>
2024-12-21 15:29:44 +0800

    feat: add footer for listing and my-applications pages

--- src/app/my-applications/layout.tsx
+++ src/app/my-applications/layout.tsx
@@ -1,12 +1,9 @@
 'use client';
 import Layout from 'pageComponents/layout';
-import { useCommonState } from 'store/Provider/hooks';
 
 const MyApplicationsLayout = ({ children }: { children: React.ReactNode }) => {
-  const { isPadPX } = useCommonState();
-
   return (
-    <Layout isShowHeader={true} isShowSider={false} isShowFooter={!isPadPX} isFullWidth={true}>
+    <Layout isShowHeader={true} isShowSider={false} isShowFooter={true} isFullWidth={true}>
       {children}
     </Layout>
   );
